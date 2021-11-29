6d9ace3730e8fec90b63eee78c61995341afca67
scottkuecker <kueckerscott@gmail.com>
2021-11-29 16:57:20 +0100

    feat: achievements improved

--- controllers/achievements.js
+++ controllers/achievements.js
@@ -1,13 +1,46 @@
 const Achievement = require('../db_models/achievement');
+const User = require('../db_models/user');
 
 exports.getAchievements =  async (req, res, next) =>{
     const achievements = await Achievement.find();
+    const user = await User.findById(req.user._id);
+    const userAchievements = user.achievements
+    const modifiedAchievements = [];
+      if (achievements && achievements.length && userAchievements && userAchievements.length){
+        for(let i = 0; i < userAchievements.length; i++){
+          for(let j = 0; j < achievements.length; j++){
+            if(userAchievements[i].category === achievements[j].category){
+              modifiedAchievements.push({
+                category: achievements[j].category,
+                achiveText: achievements[j].achiveText,
+                achievedAt: achievements[j].achievedAt,
+                answered: userAchievements[i].answered,
+                price_received: userAchievements[i].answered > achievements[j].achievedAt
+              })
+            }else{
+              modifiedAchievements.push({
+                category: achievements[j].category,
+                achiveText: achievements[j].achiveText,
+                achievedAt: achievements[j].achievedAt,
+                answered: 0,
+                price_received: false
+              })
+            }
+          }
+        }
+        return res.send({
+          success: true,
+          data: modifiedAchievements || [],
+          error: ''
+        })
 
-    return res.send({
-      success: true,
-      data: achievements || [],
-      error: ''
-    })
+      }else{
+          return res.send({
+            success: false,
+            data: [],
+            error: ''
+         })
+    }
   }
 
   exports.createAchievements = (req, res, next) =>{
