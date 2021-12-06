64aeed53919ab302e88e4962314e4726e219bb6a
scottkuecker <kueckerscott@gmail.com>
2021-12-06 13:14:29 +0100

    chore: typo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const multer = require('multer');
 
 const server = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
