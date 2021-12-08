56dfe30f622dda8961fac858b8dde3c1e5bf6a3a
scottkuecker <kueckerscott@gmail.com>
2021-12-08 11:38:39 +0100

    feat: Upload image google apis 1

--- app.js
+++ app.js
@@ -5,70 +5,49 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const middleware = require('./midleware/auth');
 const utils = require('./utils/daily-reset');
+const multer = require('multer');
+const fs = require('fs');
+const { google } = require('googleapis');
 
+const KEYFILEPATH = path.join(__dirname, 'environment.json');
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
+const auth = new google.auth.GoogleAuth({
+    keyFile: KEYFILEPATH,
+    scopes: SCOPES
+})
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const stream = fs.createReadStream(file);
+        uploadImage(file, stream)
+    }
+
+    // filename: function (req, file, cb) {
+    //     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    // }
+});
 
 const questionRoutes = require('./routes/questions-routes');
 const authRoutes = require('./routes/auth-routes');
 const userRoutes = require('./routes/user-routes');
 const achievementRoutes = require('./routes/achievement-routes');
-const multer = require('multer');
 
 const server = express();
 
 const port = process.env.PORT || 3000;
-console.log(process.env.PORT)
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + file.originalname;
-        cb(null, uniqueSuffix)
-    }
-})
-const upload = multer({storage})
-
+const driveService = google.drive({ version: 'v3', auth });
 
 server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')));
+let upload = multer({ storage: storage}).single('image');
 server.use(cors({ origin: ['http://localhost:4200', 'https://kviz-live.web.app', 'http://localhost:4201'] }));
-server.post('/add-image-question', upload.single('image'), (req, res, next) => {
-    const auth = req.get('Authorization');
-    if (!auth) {
-        return res.send({
-            success: false,
-            error: 'Not logged in'
-        })
-    }
-    const token = auth.split(' ')[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, process.env.signingSecret);
-        if(!decodedToken){
-           throw new Error('Please log in')
-        }
-    }
-    catch (e) {
-        return res.json({
-            sucess: false,
-            data: undefined,
-            error: 'Authorization failed or missing tokken'
-        })
-    }
-    if (req.file) {
-        const path = req.file.path.split('/')[1];
-        return res.send({
-            success: true,
-            data: process.env.serverAddress + path
-        })
-    }
-    return res.send({
-        success: true
-    })
+server.post('/add-image-question', (req, res, next) => {
+    upload(req)
 })
+
 server.use(questionRoutes);
 server.use(authRoutes);
 server.use(userRoutes);
@@ -80,12 +59,34 @@ server.use('', (req,res, next)=>{
         })
 })
 
+
 mongoose.connect(process.env.mongoUrl).then(() =>{
     utils.initiDailiReset();
+    console.log('connected')
     server.listen(port)
 }).catch((error)=>{
     console.error(error)
 })
+
+
+async function uploadImage(file, stream){
+    console.log(stream)
+    let fileMetaData = {
+        'name': Date.now() + file.originalname
+    }
+    // let media = {
+    //     mimeType: file.mimetype,
+    //     body: fs.createReadStream(file)
+    // }
+
+    // let response = await driveService.files.create({
+    //     resource: fileMetaData,
+    //     media: media,
+    //     fields: 'id'
+    // })
+
+    console.log(response)
+}
 
 
 
