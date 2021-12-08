315901a6c5998c5d078f72d798e62b472accb745
scottkuecker <kueckerscott@gmail.com>
2021-12-08 12:26:21 +0100

    feat: Google drive api service

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const fs = require('fs');
 const middleware = require('./midleware/auth');
 const utils = require('./utils/daily-reset');
-const multer = require('multer');
-const fs = require('fs');
 const { google } = require('googleapis');
 
 const KEYFILEPATH = path.join(__dirname, 'environment.json');
@@ -17,37 +16,76 @@ const auth = new google.auth.GoogleAuth({
     scopes: SCOPES
 })
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const stream = fs.createReadStream(file);
-        uploadImage(file, stream)
-    }
+const REFRESH_TOKEN = '';
 
-    // filename: function (req, file, cb) {
-    //     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    // }
-});
+const authorizeUrl = 'https://www.googleapis.com/auth/drive';
 
 const questionRoutes = require('./routes/questions-routes');
 const authRoutes = require('./routes/auth-routes');
 const userRoutes = require('./routes/user-routes');
 const achievementRoutes = require('./routes/achievement-routes');
+const multer = require('multer');
 
 const server = express();
 
 const port = process.env.PORT || 3000;
 
-const driveService = google.drive({ version: 'v3', auth });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public')
+    },
+    filename: async function (req, file, cb) {
+    
+        const uniqueSuffix = Date.now() + '-' + file.originalname;
+        const fileData = {
+            name: uniqueSuffix,
+            mimeType: file.mimetype
+        }
+        cb(null, uniqueSuffix)
+        await uploadImage(fileData);
+    }
+})
+const upload = multer({storage});
+
 
 server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')));
-let upload = multer({ storage: storage}).single('image');
 server.use(cors({ origin: ['http://localhost:4200', 'https://kviz-live.web.app', 'http://localhost:4201'] }));
-server.post('/add-image-question', (req, res, next) => {
-    upload(req)
+server.post('/add-image-question', upload.single('image'), (req, res, next) => {
+    const auth = req.get('Authorization');
+    if (!auth) {
+        return res.send({
+            success: false,
+            error: 'Not logged in'
+        })
+    }
+    const token = auth.split(' ')[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.signingSecret);
+        if(!decodedToken){
+           throw new Error('Please log in')
+        }
+    }
+    catch (e) {
+        return res.json({
+            sucess: false,
+            data: undefined,
+            error: 'Authorization failed or missing tokken'
+        })
+    }
+    if (req.file) {
+        const path = req.file.path.split('/')[1];
+        return res.send({
+            success: true,
+            data: process.env.serverAddress + path
+        })
+    }
+    return res.send({
+        success: true
+    })
 })
-
 server.use(questionRoutes);
 server.use(authRoutes);
 server.use(userRoutes);
@@ -59,33 +97,34 @@ server.use('', (req,res, next)=>{
         })
 })
 
-
 mongoose.connect(process.env.mongoUrl).then(() =>{
     utils.initiDailiReset();
-    console.log('connected')
     server.listen(port)
 }).catch((error)=>{
     console.error(error)
 })
+const driveService = google.drive({ version: 'v3', auth });
 
+async function uploadImage(fileData){
+    setTimeout(async ()=>{
+        let fileMetaData = {
+            'name': fileData.name
+        }
+        let media = {
+            mimeType: fileData.mimeType,
+            body: fs.createReadStream(`public/${fileData.name}`)
+        }
 
-async function uploadImage(file, stream){
-    console.log(stream)
-    let fileMetaData = {
-        'name': Date.now() + file.originalname
-    }
-    // let media = {
-    //     mimeType: file.mimetype,
-    //     body: fs.createReadStream(file)
-    // }
+        console.log(media)
 
-    // let response = await driveService.files.create({
-    //     resource: fileMetaData,
-    //     media: media,
-    //     fields: 'id'
-    // })
+        let response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            fields: 'webViewLink'
+        })
+        console.log(response)
+    }, 1000)
 
-    console.log(response)
 }
 
 
