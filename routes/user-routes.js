2eafe270f669db99a02bc4162b1454f972aa75a0
scottkuecker <kueckerscott@gmail.com>
2021-12-28 09:50:44 +0100

    chore: Checking requests

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,7 +7,7 @@ router.post('/score', middleware.authMidleware, user.updateScore);
 router.post('/name', middleware.authMidleware, user.updateName);
 router.post('/reset-lives', middleware.authMidleware, user.resetLives);
 router.post('/ranking-list', middleware.authMidleware, user.getRankingList);
-router.get('/daily-reward', middleware.authMidleware, user.resetDailyPrice);
+router.post('/daily-reward', middleware.authMidleware, user.resetDailyPrice);
 router.post('/reset-playing-state', middleware.authMidleware, user.resetPlayingState);
 
 module.exports = router;
