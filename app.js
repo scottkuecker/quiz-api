1533a93c74674e254d6228288b555be264210306
scottkuecker <kueckerscott@gmail.com>
2021-12-28 10:01:52 +0100

    chore: Testing cors 1

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const fs = require('fs');
+const server = express();
+server.use(cors());
+const port = process.env.PORT;
 
-const middleware = require('./midleware/auth');
+// const middleware = require('./midleware/auth');
 
 const questionRoutes = require('./routes/questions-routes');
 const authRoutes = require('./routes/auth-routes');
 const userRoutes = require('./routes/user-routes');
 const achievementRoutes = require('./routes/achievement-routes');
 
-const server = express();
 
-const port = process.env.PORT;
 
-server.use(cors());
 server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')));
