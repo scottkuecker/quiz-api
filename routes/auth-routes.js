a02099a94a360abfc1231914749bc772af0b8904
scottkuecker <kueckerscott@gmail.com>
2022-03-18 21:13:10 +0100

    Auth cors

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,15 +2,16 @@ const auth = require('./../controllers/auth');
 const middleware = require('../midleware/auth');
 const handler = require('../utils/errorHandler');
 
+const cors = require("cors");
 const router = require('express').Router();
 
 
 
 router.post('/signup', auth.signUp);
-router.post('/login', auth.login);
-router.post('/autologin', middleware.authMidleware, auth.autoLogin);
+router.post('/login', cors(), auth.login);
+router.post('/autologin',cors(), middleware.authMidleware, auth.autoLogin);
 router.post('/facebook-login', auth.facebookLogin);
-router.post('/refresh', middleware.authMidleware, auth.refreshUser);
+router.post('/refresh', cors(), middleware.authMidleware, auth.refreshUser);
 
 
 
