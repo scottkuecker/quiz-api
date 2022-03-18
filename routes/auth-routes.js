39729b7abd345b86f5f7e00055070db5752305d5
scottkuecker <kueckerscott@gmail.com>
2022-03-18 21:23:09 +0100

    test 20

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,9 +2,12 @@ const auth = require('./../controllers/auth');
 const middleware = require('../midleware/auth');
 const handler = require('../utils/errorHandler');
 
-const cors = require("cors");
 const router = require('express').Router();
 
+function cors(req,res,next){
+  res.header('Access-Control-Allow-Origin', 'https://kviz-live.web.app')
+  next();
+}
 
 
 router.post('/signup', auth.signUp);
