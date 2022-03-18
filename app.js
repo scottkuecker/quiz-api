c909359471455b8638593d9732d2839375ce9cd2
scottkuecker <kueckerscott@gmail.com>
2022-03-18 14:23:07 +0100

    Content Type

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 server.use("", (req, res, next) => { 
  res.setHeader("Access-Control-Allow-Origin", "*");
  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE"); 
- res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+ res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
  next();
  });
 
