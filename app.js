6481a636edd59f2ec9b1256937c8f54b8b9c4e60
scottkuecker <kueckerscott@gmail.com>
2022-03-18 14:26:51 +0100

    Options

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const port = process.env.PORT;
 
 server.use("", (req, res, next) => { 
  res.setHeader("Access-Control-Allow-Origin", "*");
- res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE"); 
- res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+ res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS"); 
+ res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
  next();
  });
 
