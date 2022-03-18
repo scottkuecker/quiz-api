54f2c787bb8cd9a2d6c5ee8fadcab2089e5c821a
scottkuecker <kueckerscott@gmail.com>
2022-03-18 21:33:45 +0100

    test 22

--- midleware/auth.js
+++ midleware/auth.js
@@ -44,7 +44,7 @@ exports.authMidleware = (req, res,next) =>{
    
 }
 exports.headers = (req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://kviz-live.web.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
