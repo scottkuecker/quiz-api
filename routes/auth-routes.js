2266369ae3316bd9218a4fac6a9c498e0434c7aa
scottkuecker <kueckerscott@gmail.com>
2022-03-19 03:47:14 +0100

    Refresh

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,9 +6,9 @@ const router = require('express').Router();
 
 router.post('/signup', auth.signUp);
 router.post('/login', auth.login);
-router.post('/autologin',middleware.headers, middleware.authMidleware, auth.autoLogin);
+router.post('/autologin, middleware.authMidleware, auth.autoLogin);
 router.post('/facebook-login', auth.facebookLogin);
-router.post('/refresh' ,middleware.headers, middleware.authMidleware, auth.refreshUser);
+router.post('/refresh', auth.refreshUser);
 
 
 
